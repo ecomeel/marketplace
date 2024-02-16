3fc1ea193a4d6c4edc15db8fee51f97c33dd3389
Aleksandr Pulin <ecomeel@gmail.com>
2024-02-16 23:02:26 +0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <h1 className="logo">Marketplace</h1>
+      <div className="search">
+        <img src="" alt="" />
+        <input type="text" />
+      </div>
+    </header>
+  );
+}
