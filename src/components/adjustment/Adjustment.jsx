71743ed85fdb2c600e03784144cfb7919a4f556f
Aleksandr Pulin <ecomeel@gmail.com>
2024-02-17 13:59:55 +0300

    implements range sliders

--- src/components/adjustment/Adjustment.jsx
+++ src/components/adjustment/Adjustment.jsx
@@ -1,11 +1,12 @@
 import { Adjuster } from "./adjuster/Adjuster";
+
 import styles from "./adjustment.module.scss";
 export function Adjustment() {
   return (
     <section className={styles.adjustment}>
-      <Adjuster />
-      <Adjuster />
-      <Adjuster />
+      <Adjuster title="Price Range" from="234" to="9999" type="$" />
+      <Adjuster title="THC, %" from="0" to="100" type="%" />
+      <Adjuster title="CBD, %" from="0" to="100" type="%" />
     </section>
   );
 }
--- src/components/adjustment/adjuster/Adjuster.jsx
+++ src/components/adjustment/adjuster/Adjuster.jsx
@@ -1,5 +1,40 @@
-export function Adjuster() {
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
+
+import styles from "./adjuster.module.scss";
+export function Adjuster({ id, title, from, to, type }) {
+  const sliderNode = document.getElementById(`slider${id}`);
+  if (sliderNode) {
+    noUiSlider.create(sliderNode, {
+      start: [20, 80],
+      connect: true,
+      range: {
+        min: 0,
+        max: 100,
+      },
+    });
+  }
+
   return (
-    <h1>Adjuster</h1>
-  )
+    <div className={styles.adjuster}>
+      <p className={styles.title}>{title}</p>
+      <div className={styles.slider}>
+        <RangeSlider defaultValue={[from, to]} min={from} max={to} />
+      </div>
+      <div className={styles.inputs}>
+        <input
+          className={styles.input}
+          placeholder={`${from} ${type}`}
+          type="number"
+          id=""
+        />
+        <input
+          className={styles.input}
+          placeholder={`${to} ${type}`}
+          type="number"
+          id=""
+        />
+      </div>
+    </div>
+  );
 }
